2963a90832e0964e3a2c724fc7df4bec442cf47b
Mattan.M <mattan.m@synectmedia.com>
2019-06-06 11:52:24 -0700

    first commit

---
+++ Controllers/ImageBankApiController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HtmlToImageDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageBankApiController : ControllerBase
+    {
+        private const string BaseDir = "wwwroot";
+        private const string DirName = "images";
+        private const string ImageName = "image.png";
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var fileName = Path.Combine(Directory.GetCurrentDirectory(), BaseDir, DirName, ImageName);
+            if (System.IO.File.Exists(fileName))
+            {
+                var url = $"{HttpContext.Request.Host}/{DirName}/{ImageName}";
+                
+                return Ok(url);
+            }
+            else
+            {
+                return BadRequest("No image is currently stored.");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(BinaryImageModel binaryImageModel)
+        {
+            try
+            {
+                var res = SaveImage(binaryImageModel.Data);
+
+                return Ok(res);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        private bool SaveImage(string imageData)
+        {            
+            var directory = Path.Combine(Directory.GetCurrentDirectory(), BaseDir, DirName);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            var imageStream = ConvertBase64ToImage(imageData);
+
+            using (Stream file = System.IO.File.Create(Path.Combine(directory, ImageName)))
+            {
+                CopyStream(imageStream, file);
+
+                imageStream.Close();
+                file.Close();
+            }
+
+            return true;
+        }
+
+        private Stream ConvertBase64ToImage(string base64String)
+        {
+            byte[] data = Convert.FromBase64String(base64String);
+            return new MemoryStream(data, 0, data.Length);
+        }
+
+        private void CopyStream(Stream input, Stream output)
+        {
+            byte[] buffer = new byte[8 * 1024];
+            int len;
+            while ((len = input.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                output.Write(buffer, 0, len);
+            }
+        }
+    }
+
+    public class BinaryImageModel
+    {
+        private string _data;
+        public string Data
+        {
+            get
+            {
+                return _data;
+            }
+            set { _data = value.Substring(value.IndexOf(',') + 1); }
+        }
+    }
+}
---
+++ wwwroot/js/scripts.js
@@ -0,0 +1,59 @@
+﻿$(document).ready(() => {    
+    $("#btn-snap").click(() => {
+        html2canvas(document.body, {
+            background: undefined,
+            onrendered: function (canvas) {
+                var image = canvas.toDataURL("image/png");
+                var url = "api/ImageBankApi";
+                var payload = {
+                    Data: image
+                };
+                $.ajax({
+                    url: url,
+                    type: "POST",
+                    data: JSON.stringify(payload),
+                    contentType: "application/json",
+                    success: showSuccessfulSnap,
+                    error: function (err) {
+                        console.log(JSON.stringify(err));
+                    }
+                });
+            }
+        });
+    });
+
+    $("#btn-download").click(() => {
+        downloadURI("https://localhost:44374/images/image.png", "image.png");
+    })
+
+    $.getJSON("https://localhost:44374/images/image.png",
+        () => $("#btn-download").removeClass("disabled"))    
+    .fail(() => $("#btn-download").removeClass("disabled"));
+});
+
+function showSuccessfulSnap() {    $("#snap-success").show();    
+    setTimeout(() => $("#snap-success").fadeOut(1000), 2000);    
+}
+
+function getClock() {
+    var d = new Date();
+    var hours = d.getHours().toString().padStart(2, '0');
+    var minutes = d.getMinutes().toString().padStart(2, '0');
+    var seconds = d.getSeconds().toString().padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(() => {
+    $("#clock").text(getClock());
+}, 1000);
+$("#clock").text(getClock());
+
+function downloadURI(uri, name) {
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    delete link;
+}
